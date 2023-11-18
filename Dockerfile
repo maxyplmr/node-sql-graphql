0094c3687103b871a90941a699ccdc7e22575c49
Maksym Palamar <maxyplmr@gmail.com>
2023-11-18 19:25:40 +0200

    - docker file fixes

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ RUN npm install
 COPY . .
 
 # Expose the port that your application will run on
-EXPOSE 3000
+EXPOSE 2999
 
 # Start the application
-CMD ["node", "index.js"]
+CMD ["node", "server.js"]
--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'mariadb',
     user: 'username',
     password: 'userPassword',
     database: 'databaseName',
